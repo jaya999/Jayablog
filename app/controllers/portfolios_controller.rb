cb94aabbe6222536e47192a73d154c9c2dcc3587
jaya999 <jayaror1991@gmail.com>
2018-05-29 15:32:28 +0530

    Implemanted custom scopes for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,6 +3,10 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+
   def new
     @portfolio_item = Portfolio.new
   end
@@ -25,7 +29,7 @@ class PortfoliosController < ApplicationController
 
   def update
     @portfolio_item = Portfolio.find(params[:id])
-    
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -35,18 +39,18 @@ class PortfoliosController < ApplicationController
     end
   end
 
-  def show 
+  def show
     @portfolio_item = Portfolio.find(params[:id])
   end
 
   def destroy
     # Perform the lookup
-   @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
 
-      # Destroy/delete the record
+    # Destroy/delete the record
     @portfolio_item.destroy
 
-      # Redirect
+    # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
